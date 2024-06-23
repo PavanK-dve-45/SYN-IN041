d8fb3757319a534fd7f4ae5b1a5c5e6adef9e331
PavanK dev 45 <pavankumarhk411@gmail.com>
2024-06-23 13:37:46 +0530

    Java code to determine prime number

---
+++ Prime.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        System.out.println("Enter a Number: ");
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        sc.close();
+
+    }
+}
